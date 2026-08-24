2beffb8937c1de62c29c125a6789985fef6af055
cumar cabdi cali <Omarcabdi0008@gmail.com>
2026-08-24 08:32:20 -0700

    14 updating ldatabase link

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL
+    await mongoose.connect(process.env.MONGODB_URI
     );
     console.log('Connected to MongoDB');
   } catch (err) {
