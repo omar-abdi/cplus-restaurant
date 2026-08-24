fd8d92719c799ad15cb4921c7043a3a8efe67936
cumar cabdi cali <Omarcabdi0008@gmail.com>
2026-08-24 08:32:52 -0700

    15 using cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db.js';
+import cookieParser from 'cookie-parser';
+import userRoutes from './routes/user.js';
+import foodRoutes from './routes/food.js';
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 import dns from "dns";
 dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 8000;
+app.use(cookieParser());
+app.use(express.json())
 
+
+const PORT = process.env.PORT || 8000;
+app.use('/api/user', userRoutes);
+app.use('/api/food', foodRoutes);
 app.listen(PORT, () => {
     connectDB();
   console.log(`Server is running on port ${PORT}`);
