43eea354937ceb05a377e2a906f72046325381d5
cumar cabdi cali <Omarcabdi0008@gmail.com>
2026-08-24 07:36:35 -0700

    4 connection database

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
-
-
+import connectDB from './db.js';
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+import dns from "dns";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
+    connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
 
