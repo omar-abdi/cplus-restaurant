ce19cc981e569c6d145067eec8d00677d5ceb53f
cumar cabdi cali <Omarcabdi0008@gmail.com>
2026-08-24 07:58:46 -0700

    10 only admin route

---
+++ backend/onliAdmin.js
@@ -0,0 +1,11 @@
+const adminOnly = (req, res, next) => {
+  if (!req.user || !req.user.isAdmin) {
+    return res.status(403).json({
+      message: 'Admin access only',
+    });
+  }
+
+  next();
+};
+
+export default adminOnly;
