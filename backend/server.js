ea5d33db2782bd42480016743f2bcbba0c4da163
cumar cabdi cali <Omarcabdi0008@gmail.com>
2026-08-26 11:14:22 -0700

    update server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db.js';
@@ -9,30 +8,31 @@ import drinksRoutes from './routes/drinks.js';
 import orderRoutes from './routes/order.js';
 import contactRoutes from './routes/contact.js';
 import cors from 'cors';
+import dns from 'dns';
+
+// DNS Fix MongoDB Atlas SRV Error
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-import dns from "dns";
 dotenv.config();
+
 const app = express();
+
 app.use(cookieParser());
-app.use(express.json())
+app.use(express.json());
 app.use(cors({
   origin: true,
   credentials: true,
 }));
 
-
 const PORT = process.env.PORT || 8000;
+
 app.use('/api/user', userRoutes);
 app.use('/api/food', foodRoutes);
 app.use('/api/drinks', drinksRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/contact', contactRoutes);
-app.listen(PORT, () => {
-    connectDB();
+
+app.listen(PORT, async () => {
+  await connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
-
-
-
-
