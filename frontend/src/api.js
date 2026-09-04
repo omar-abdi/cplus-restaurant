b039ac244757d20c786b1dab0c988113523a310f
cumar cabdi cali <Omarcabdi0008@gmail.com>
2026-09-04 05:52:30 -0700

    update local host

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({
   baseURL: import.meta.env.MODE === 'production' 
     ? '/api' 
-    : 'http://localhost:8000/api', // Hubi in uu 8000 ama 5000 yahay port-kaaga local-ka
+    : 'http://localhost:5000/api', // Hubi in uu 8000 ama 5000 yahay port-kaaga local-ka
   withCredentials: true,
 });
 
